115d2fe61a4d75fd7e4fb03fa6cdf6bc2f6b9df9
304089 <9457imai2@gmail.com>
2021-12-02 17:39:01 +0900

    [create]ヘッダーコンテンツ

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+// 画像をそれぞれimportします。パスに注意してください。
+import CoverImage from '../images/cover-image.JPG';
+import ProfileImage from '../images/profile-image.JPG';
+
+export const Header = () => {
+  return (
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            <h1 className="title-text">YUICHIROU IMAI</h1>
+            <h3 className="title-text">former job: Civil servant</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
